45f9e2656c8c585b86af9a25e81300358f10dbf1
Dmitry Savransky <dsavransky@gmail.com>
2020-07-07 15:44:11 -0400

    updating setup

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 import setuptools
-import os, re
+import os
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open(os.path.join("cornellGrading","__init__.py"), "r") as f:
+with open(os.path.join("cornellGrading", "__init__.py"), "r") as f:
     version_file = f.read()
 
-version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",\
-        version_file, re.M)
+version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
 
 if version_match:
     version_string = version_match.group(1)
 else:
     raise RuntimeError("Unable to find version string.")
-
 
 
 setuptools.setup(
@@ -26,22 +25,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dsavransky/grading",
-    packages=['cornellGrading'],
-    install_requires=[
-          'numpy',
-          'pandas',
-          'keyring',
-          'pytz',
-          'canvasapi',
-          'requests',
-          
-    ],
-    extras_require={
-        "latex2html":  ["pdf2image", "Pillow"]
-    },
+    packages=["cornellGrading"],
+    install_requires=["numpy", "pandas", "keyring", "pytz", "canvasapi", "requests",],
+    extras_require={"latex2html": ["pdf2image", "Pillow"]},
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],

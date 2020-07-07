fe96fe0b832559578f5f1c43c4e28de867041a87
Dmitry Savransky <dsavransky@gmail.com>
2020-07-07 15:30:50 -0400

    blacked everything and fixed errors in utilities.  made version 1.0.0 for initial PyPI ingest

--- cornellGrading/__init__.py
+++ cornellGrading/__init__.py
@@ -1,3 +1,3 @@
-name = "MeanStars"
-__version__ = '1.1.0'
+name = "cornellGrading"
+__version__ = '1.0.0'
 from .cornellGrading import cornellGrading
--- cornellGrading/matlabgrader.py
+++ cornellGrading/matlabgrader.py
@@ -8,23 +8,32 @@ In Grader:
    - Save file
 """
 
-import cornellGrading 
+import cornellGrading
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Parse MATLAB Grader output and upload to Canvas")
-    parser.add_argument('assignmentNum', nargs=1, type=int, help='Number of assignment (int).')
-    parser.add_argument('gradercsv', nargs=1, type=str, help='Full path to grader export (string).')
-    parser.add_argument('duedate', nargs=1, type=str, help='Assignment due date (string: YYYY-MM-DD).')
+    parser = argparse.ArgumentParser(
+        description="Parse MATLAB Grader output and upload to Canvas"
+    )
+    parser.add_argument("courseNum", nargs=1, type=int, help="Canvas course id (int).")
+    parser.add_argument(
+        "assignmentNum", nargs=1, type=int, help="Number of assignment (int)."
+    )
+    parser.add_argument(
+        "gradercsv", nargs=1, type=str, help="Full path to grader export (string)."
+    )
+    parser.add_argument(
+        "duedate", nargs=1, type=str, help="Assignment due date (string: YYYY-MM-DD)."
+    )
 
     args = parser.parse_args()
 
+    coursenum = args.courseNum[0]
     gradercsv = args.gradercsv[0]
     assignmentNum = args.assignmentNum[0]
     duedate = args.duedate[0]
 
-    c = cornellGrading.cornellGrading() 
-    c.getCourse()
+    c = cornellGrading.cornellGrading()
+    c.getCourse(coursenum)
 
-    c.matlabImport(assignmentNum,gradercsv,duedate)
-
+    c.matlabImport(assignmentNum, gradercsv, duedate)

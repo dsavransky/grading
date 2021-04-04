0117c83e0c51be7e93aa1cfc57bcf02ee55c3cad
afoarce <foarceandreea@gmail.com>
2021-04-04 15:02:01 -0400

    update imports and platform logic

--- cornellGrading/__init__.py
+++ cornellGrading/__init__.py
@@ -2,4 +2,4 @@ name = "cornellGrading"
 __version__ = "2.8.2"
 from .cornellGrading import cornellGrading
 from .cornellQualtrics import cornellQualtrics
-from .cornellGrading import cornellInterface
+from .cornellInterface import cornellInterface
